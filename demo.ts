dc09471e377b3a62b074fc08146cd490a644176a
cjshinningg <784319417@qq.com>
2020-11-08 13:13:09 +0800

    2-5 静态类型深度理解

--- demo.ts
+++ demo.ts
@@ -1,16 +1,11 @@
-interface Point {
-  x: number
-  y: number
-}
+// interface Point {
+//     x: number,
+//     y: number
+// }
 
-function tsDemo(data: Point) {
-  console.log('Dell ts')
-  return Math.sqrt(data.x ** 2 + data.y ** 2)
-}
+// const point: Point = {
+//     x: 3,
+//     y: 4
+// };
 
-tsDemo({ x: 1, y: 123 })
-
-// typescript优势：
-// 1.开发过程中，发现潜在问题
-// 2.更友好的编辑器自动提示
-// 3.代码可读性好
+// point.x
