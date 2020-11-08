18c4daef7c9c7365749673dd3996c4f6417ecf9c
cjshinningg <784319417@qq.com>
2020-11-08 17:51:14 +0800

    2-8 函数相关类型

--- demo.ts
+++ demo.ts
@@ -1,24 +1,22 @@
-// type annotation 类型注解，我们来告诉TS变量是什么类型
-// type inference 类型推断，TS会自动的去尝试分析变量的类型
-// 如果TS能够自动分析变量类型，我们就什么也不需要做了
-// 如果TS无法分析变量类型的话，我们就需要使用类型注解
+// function add(first: number, second: number): number {
+//   return first + second
+// }
 
-// let count: number
-// count = 123
+// function sayHello(): void {
+//   console.log('hello')
+// }
 
-// let countInference = 123
+// function errorEmitter(): never {
+//   throw new Error()
+// }
 
-// const firstNumber = 1;
-// const secondNumer = 2;
-// const total = firstNumber + secondNumer
-
-function getTotal(firstNumber: number, secondNumer: number) {
-  return firstNumber + secondNumer
+function add({ first, second }: { first: number; second: number }): number {
+  return first + second
 }
 
-const total = getTotal(1, 2)
-
-const obj = {
-  name: 'dell',
-  age: 18
+function getNumber({ first }: { first: number }) {
+  return first
 }
+
+const total = add({ first: 1, second: 2 })
+const count = getNumber({ first: 1 })
