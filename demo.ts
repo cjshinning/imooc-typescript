56e9f3959384856001032590801d4e8edfc0a186
cjshinningg <784319417@qq.com>
2020-12-20 23:04:20 +0800

    2-14 静态属性，setter和getter

--- demo.ts
+++ demo.ts
@@ -1,52 +1,32 @@
-// private, protected, public
-// public 允许我在类的内外被调用
-// private 允许在类内被使用
-// protected 允许在类内及继承的子类中使用
 // class Person {
-//   public name: string = '';
-//   public sayHi() {
-//     this.name;
-//     console.log('hi');
+//   constructor(private _name: string = '') {}
+//   get name() {
+//     return this._name + ' chan';
 //   }
-//   private sayABC() {
-//     this.sayHi();
-//   }
-// }
-
-// class Teacher extends Person {
-//   public sayBye() {
-//     this.sayHi();
+//   set name(name: string) {
+//     const realName = name.split(' ')[0];
+//     this._name = realName;
 //   }
 // }
 
-// const person = new Person();
-// person.name = 'jenny';
-// console.log(person.name);
-// person.sayHi();
-
-// constructor
-// class Person {
-//   // 传统写法
-//   // public name: string = '';
-//   // constructor(name: string = '') {
-//   //   this.name = name;
-//   // }
-//   // 简化写法
-//   constructor(public name: string = '') {}
-// }
 // const person = new Person('jenny');
 // console.log(person.name);
+// person.name = 'jenny chan';
+// console.log(person.name);
 
-class Person {
-  // constructor(public name: string = '') {}
-}
+class Demo {
+  private static instance: Demo;
+  private constructor(public name: string) {}
 
-class Teacher extends Person {
-  constructor(public age: number) {
-    super();
+  public static getInstance() {
+    if (!this.instance) {
+      this.instance = new Demo('jenny chan');
+    }
+    return this.instance;
   }
 }
 
-const teacher = new Teacher(28);
-console.log(teacher.age);
-// console.log(teacher.name);
+const demo1 = Demo.getInstance();
+const demo2 = Demo.getInstance();
+console.log(demo1.name);
+console.log(demo2.name);
