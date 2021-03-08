e08361e6b4be43156463cc01f9a4d15ce1ac6082
cjshinningg <784319417@qq.com>
2021-03-08 19:12:35 +0800

    5-5 统一接口数据格式，优化代码

--- build/router.js
+++ build/router.js
@@ -3,11 +3,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var crawller_1 = __importDefault(require("./crawller"));
-var jennyAnalyzer_1 = __importDefault(require("./jennyAnalyzer"));
 var fs_1 = __importDefault(require("fs"));
 var path_1 = __importDefault(require("path"));
+var express_1 = require("express");
+var crawller_1 = __importDefault(require("./utils/crawller"));
+var analyzer_1 = __importDefault(require("./utils/analyzer"));
+var util_1 = require("./utils/util");
+var checkLogin = function (req, res, next) {
+    var isLogin = req.session ? req.session.login : false;
+    if (isLogin) {
+        next();
+    }
+    else {
+        res.json(util_1.getResponseData(null, '请先登录'));
+    }
+};
 var router = express_1.Router();
 router.get('/', function (req, res) {
     var isLogin = req.session ? req.session.login : false;
@@ -22,51 +32,39 @@ router.get('/logout', function (req, res) {
     if (req.session) {
         req.session.login = undefined;
     }
-    res.redirect('/');
+    res.json(util_1.getResponseData(true));
 });
 router.post('/login', function (req, res) {
     var password = req.body.password;
     var isLogin = req.session ? req.session.login : false;
     if (isLogin) {
-        res.send('已经登录过');
+        res.json(util_1.getResponseData(false, '已经登录过'));
     }
     else {
         if (password === '123' && req.session) {
             req.session.login = true;
-            res.send('登录成功');
+            res.json(util_1.getResponseData(true));
         }
         else {
-            res.send('登录失败');
+            res.json(util_1.getResponseData(false, '登录失败'));
         }
     }
 });
-router.get('/getData', function (req, res) {
-    var isLogin = req.session ? req.session.login : false;
-    if (isLogin) {
-        var secret = 'x3b174jsx';
-        var url = "http://www.dell-lee.com/typescript/demo.html?secret=" + secret;
-        var analyzer = jennyAnalyzer_1.default.getInstance();
-        new crawller_1.default(url, analyzer);
-        res.send('getData Success!');
-    }
-    else {
-        res.send("\u8BF7\u767B\u5F55\u540E\u722C\u53D6\u5185\u5BB9");
-    }
+router.get('/getData', checkLogin, function (req, res) {
+    var secret = 'x3b174jsx';
+    var url = "http://www.dell-lee.com/typescript/demo.html?secret=" + secret;
+    var analyzer = analyzer_1.default.getInstance();
+    new crawller_1.default(url, analyzer);
+    res.json(util_1.getResponseData(true));
 });
-router.get('/showData', function (req, res) {
-    var isLogin = req.session ? req.session.login : false;
-    if (isLogin) {
-        try {
-            var position = path_1.default.resolve(__dirname, '../data/course.json');
-            var result = fs_1.default.readFileSync(position, 'utf8');
-            res.json(JSON.parse(result));
-        }
-        catch (e) {
-            res.send('尚未爬取到内容');
-        }
+router.get('/showData', checkLogin, function (req, res) {
+    try {
+        var position = path_1.default.resolve(__dirname, '../data/course.json');
+        var result = fs_1.default.readFileSync(position, 'utf8');
+        res.json(util_1.getResponseData(JSON.parse(result)));
     }
-    else {
-        res.send('请登录后查看内容');
+    catch (e) {
+        res.json(util_1.getResponseData(false, '数据不存在'));
     }
 });
 exports.default = router;
--- src/router.ts
+++ src/router.ts
@@ -1,14 +1,22 @@
-import { Router, Request, Response } from 'express';
-import Crawller from './crawller';
-import JennyAnalyzer from './jennyAnalyzer';
 import fs from 'fs';
 import path from 'path';
+import { Router, Request, Response, NextFunction } from 'express';
+import Crawller from './utils/crawller';
+import Analyzer from './utils/analyzer';
+import { getResponseData } from './utils/util';
 
-interface RequestWithBody extends Request {
-  body: {
-    [key: string]: string | undefined;
-  };
+interface BodyRequest extends Request {
+  body: { [key: string]: string | undefined };
 }
+
+const checkLogin = (req: Request, res: Response, next: NextFunction) => {
+  const isLogin = req.session ? req.session.login : false;
+  if (isLogin) {
+    next();
+  } else {
+    res.json(getResponseData(null, '请先登录'));
+  }
+};
 
 const router = Router();
 
@@ -42,50 +50,39 @@ router.get('/logout', (req: Request, res: Response) => {
   if (req.session) {
     req.session.login = undefined;
   }
-  res.redirect('/');
+  res.json(getResponseData(true));
 });
 
-router.post('/login', (req: RequestWithBody, res: Response) => {
+router.post('/login', (req: BodyRequest, res: Response) => {
   const { password } = req.body;
   const isLogin = req.session ? req.session.login : false;
-
   if (isLogin) {
-    res.send('已经登录过');
+    res.json(getResponseData(false, '已经登录过'));
   } else {
     if (password === '123' && req.session) {
       req.session.login = true;
-      res.send('登录成功');
+      res.json(getResponseData(true));
     } else {
-      res.send('登录失败');
+      res.json(getResponseData(false, '登录失败'));
     }
   }
 });
 
-router.get('/getData', (req: RequestWithBody, res: Response) => {
-  const isLogin = req.session ? req.session.login : false;
-  if (isLogin) {
-    const secret = 'x3b174jsx';
-    const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`;
-    const analyzer = JennyAnalyzer.getInstance();
-    new Crawller(url, analyzer);
-    res.send('getData Success!');
-  } else {
-    res.send(`请登录后爬取内容`);
-  }
+router.get('/getData', checkLogin, (req: BodyRequest, res: Response) => {
+  const secret = 'x3b174jsx';
+  const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`;
+  const analyzer = Analyzer.getInstance();
+  new Crawller(url, analyzer);
+  res.json(getResponseData(true));
 });
 
-router.get('/showData', (req: RequestWithBody, res: Response) => {
-  const isLogin = req.session ? req.session.login : false;
-  if (isLogin) {
-    try {
-      const position = path.resolve(__dirname, '../data/course.json');
-      const result = fs.readFileSync(position, 'utf8');
-      res.json(JSON.parse(result));
-    } catch (e) {
-      res.send('尚未爬取到内容');
-    }
-  } else {
-    res.send('请登录后查看内容');
+router.get('/showData', checkLogin, (req: BodyRequest, res: Response) => {
+  try {
+    const position = path.resolve(__dirname, '../data/course.json');
+    const result = fs.readFileSync(position, 'utf8');
+    res.json(getResponseData(JSON.parse(result)));
+  } catch (e) {
+    res.json(getResponseData(false, '数据不存在'));
   }
 });
 
